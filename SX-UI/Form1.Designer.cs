e6c39ee536cc477e723c8a835d3c2b4f9b861b4c
ShadowDara <shadowdara74x@gmail.com>
2026-03-15 14:35:04 +0100

    Added -

--- SX-UI/Form1.Designer.cs
+++ SX-UI/Form1.Designer.cs
@@ -231,7 +231,6 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
-            this.MinimizeBox = false;
             this.Name = "Form1";
             this.Text = "SX-UI Wrapper";
             this.Load += new System.EventHandler(this.Form1_Load);
